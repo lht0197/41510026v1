bdaf54d05f143675f4debddb6b9d44d3a03f3903
lht0197 <lht0197@qq.com>
2018-09-29 20:35:41 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/a41510026/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.administrator.a41510026;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "Rate";
+    private float dollarRate = 0.1f;
+    private float euroRate = 0.2f;
+    private float wonRate = 0.3f;
+
+    EditText rmb;
+    TextView show;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SharedPreferences sharedPreferences = getSharedPreferences("myrate", Activity.MODE_PRIVATE);
+        dollarRate = sharedPreferences.getFloat("dollar_rate",0.0f);
+        euroRate = sharedPreferences.getFloat("euro_rate",0.0f);
+        wonRate = sharedPreferences.getFloat("won_rate",0.0f);
+        Log.i(TAG, "onCreate: sp dollarRate=" + dollarRate);
+        Log.i(TAG, "onCreate: sp euroRate=" + euroRate);
+        Log.i(TAG, "onCreate: sp wonRate=" + wonRate);
+    }
+
+    protected  void onClick (View V){
+        setContentView(R.layout.activity_main);
+        rmb = (EditText) findViewById(R.id.rmb);
+        show = (TextView) findViewById(R.id.show);
+
+        Log.i(TAG, "onClick: ");
+        String str = rmb.getText().toString();
+        Log.i(TAG, "onClick: get str=" + str);
+        float r = 0;
+        if(str.length()>0){
+            r = Float.parseFloat(str);
+        }else{
+//用户没有输入内容
+            Toast.makeText(this, "请输入内容", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Log.i(TAG, "onClick: r=" + r);
+
+        //计算
+        if(V.getId()==R.id.dollar){
+            show.setText(String.valueOf(r*dollarRate));
+        }else if(V.getId()==R.id.euro){
+            show.setText(String.valueOf(r*euroRate));
+        }else{
+            show.setText(String.valueOf(r*wonRate));
+        }
+    }
+
+    public void openOne(View btn){
+        Intent config = new Intent(this,ConfigActivity.class);
+        config.putExtra("dollar_rate_key",dollarRate);
+        config.putExtra("euro_rate_key",euroRate);
+        config.putExtra("won_rate_key",wonRate);
+        Log.i(TAG, "openOne: dollarRate=" + dollarRate);
+        Log.i(TAG, "openOne: euroRate=" + euroRate);
+        Log.i(TAG, "openOne: wonRate=" + wonRate);
+//startActivity(config);
+        startActivityForResult(config,1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==1 && resultCode==2){
+            Bundle bundle = data.getExtras();
+            dollarRate = bundle.getFloat("key_dollar",0.1f);
+            euroRate = bundle.getFloat("key_euro",0.1f);
+            wonRate = bundle.getFloat("key_won",0.1f);
+            Log.i(TAG, "onActivityResult: dollarRate=" + dollarRate);
+            Log.i(TAG, "onActivityResult: euroRate=" + euroRate);
+            Log.i(TAG, "onActivityResult: wonRate=" + wonRate);
+        }
+        //将新设置的汇率写到SP⾥
+        SharedPreferences sharedPreferences = getSharedPreferences("myrate", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putFloat("dollar_rate",dollarRate);
+        editor.putFloat("euro_rate",euroRate);
+        editor.putFloat("won_rate",wonRate);
+        editor.commit();
+        Log.i(TAG, "onActivityResult: 数据已保存到sharedPreferences");
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
